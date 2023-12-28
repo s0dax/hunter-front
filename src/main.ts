03cb6b821fb2e65ee55d485fd99cb6a03f053acc
s0dax <1620371944@qq.com>
2023-12-28 21:55:42 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import App from './App.vue'
 import naive from 'naive-ui'
 import qs from 'qs'
 
+
 const app = createApp(App)
 app.use(naive).use(store).mount('#app')
 app.config.globalProperties.$qs = qs;
